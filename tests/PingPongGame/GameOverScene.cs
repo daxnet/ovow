cef06f2614ab5b59d175374d3928b1a4c9159dd3
daxnet <daxnet@outlook.com>
2020-05-29 20:23:34 +0800

    Fixing compiling issues.

--- tests/PingPongGame/GameOverScene.cs
+++ tests/PingPongGame/GameOverScene.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Ovow.Framework;
+using Ovow.Framework.Components;
 using Ovow.Framework.Scenes;
 using Ovow.Framework.Sprites;
 
